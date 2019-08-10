a788cb3999cd1877d56e1318d508be7fb83beffa
Julian Rüth <julian.rueth@fsfe.org>
2019-08-10 20:18:06 +0300

    fix indent

--- arf/load_str.c
+++ arf/load_str.c
@@ -82,68 +82,68 @@ arf_dump_str(const arf_t x)
 int
 arf_load_str(arf_t x, const char* data)
 {
-  int err = 0;
+    int err = 0;
 
-  if (strcmp(data, arf_serialized_zero) == 0)
-  {
-    arf_zero(x);
-  }
-  else if (strcmp(data, arf_serialized_neg_inf) == 0)
-  {
-    arf_neg_inf(x);
-  }
-  else if (strcmp(data, arf_serialized_pos_inf) == 0)
-  {
-    arf_pos_inf(x);
-  }
-  else if (strcmp(data, arf_serialized_nan) == 0)
-  {
-    arf_nan(x);
-  }
-  else
-  {
-    fmpz_t mantissa, exponent;
-    char * e_str;
-    char * m_str;
-
-    fmpz_init(mantissa);
-    fmpz_init(exponent);
-
-    e_str = strchr(data, ' ');
-    if (e_str == NULL)
+    if (strcmp(data, arf_serialized_zero) == 0)
     {
-      return 1;
+        arf_zero(x);
+    }
+    else if (strcmp(data, arf_serialized_neg_inf) == 0)
+    {
+        arf_neg_inf(x);
+    }
+    else if (strcmp(data, arf_serialized_pos_inf) == 0)
+    {
+        arf_pos_inf(x);
+    }
+    else if (strcmp(data, arf_serialized_nan) == 0)
+    {
+        arf_nan(x);
+    }
+    else
+    {
+        fmpz_t mantissa, exponent;
+        char * e_str;
+        char * m_str;
+
+        fmpz_init(mantissa);
+        fmpz_init(exponent);
+
+        e_str = strchr(data, ' ');
+        if (e_str == NULL)
+        {
+            return 1;
+        }
+
+        m_str = (char*)flint_malloc(e_str - data + 1);
+        strncpy(m_str, data, e_str - data);
+        m_str[e_str - data] = '\0';
+
+        err = fmpz_set_str(mantissa, m_str, 16);
+
+        flint_free(m_str);
+
+        if (err)
+        {
+            fmpz_clear(exponent);
+            fmpz_clear(mantissa);
+            return err;
+        }
+
+        err = fmpz_set_str(exponent, e_str + 1, 16);
+
+        if (err)
+        {
+            fmpz_clear(exponent);
+            fmpz_clear(mantissa);
+            return err;
+        }
+
+        arf_set_fmpz_2exp(x, mantissa, exponent);
+
+        fmpz_clear(exponent);
+        fmpz_clear(mantissa);
     }
 
-    m_str = (char*)flint_malloc(e_str - data + 1);
-    strncpy(m_str, data, e_str - data);
-    m_str[e_str - data] = '\0';
-
-    err = fmpz_set_str(mantissa, m_str, 16);
-
-    flint_free(m_str);
-
-    if (err)
-    {
-      fmpz_clear(exponent);
-      fmpz_clear(mantissa);
-      return err;
-    }
-
-    err = fmpz_set_str(exponent, e_str + 1, 16);
-
-    if (err)
-    {
-      fmpz_clear(exponent);
-      fmpz_clear(mantissa);
-      return err;
-    }
-
-    arf_set_fmpz_2exp(x, mantissa, exponent);
-
-    fmpz_clear(exponent);
-    fmpz_clear(mantissa);
-  }
-
-  return err;
+    return err;
 }
